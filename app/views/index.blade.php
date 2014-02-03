d3d28f18758ed5167a2090f89b0a3dc2d996b181
Marcin <marcin19962@onet.eu>
2014-02-03 15:54:04 +0100

    Index template changes

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,24 +1,66 @@
 @extends('master')
 
 @section('content')
-<div class="jumbotron" style="height: 100%; background-color: #b9ad44">
+<div class="jumbotron" style="height: 100%; background-color: #fffa58">
     <div class="container">
-        <h1>Group A</h1>
+        <h2>Group A</h2>
+    </div>
+    <div class="container text-center">
+        <div class="row center-block" style="background-color: #888888">
+            <div class="col-md-2">Team</div>
+            <div class="col-md-2">Played Matches</div>
+            <div class="col-md-2">Matches Won</div>
+            <div class="col-md-2">Game Won</div>
+            <div class="col-md-2">Matches Lose</div>
+            <div class="col-md-2">Game Lose</div>
+        </div>
+        <div class="row center-block" style="background-color: #adadad">
+            <div class="col-md-2">Taiwan</div>
+            <div class="col-md-2">1</div>
+            <div class="col-md-2">3</div>
+            <div class="col-md-2">2</div>
+            <div class="col-md-2">8</div>
+            <div class="col-md-2">3</div>
+        </div>
+        <div class="row center-block" style="background-color: #adadad">
+            <div class="col-md-2">Taiwan</div>
+            <div class="col-md-2">1</div>
+            <div class="col-md-2">3</div>
+            <div class="col-md-2">2</div>
+            <div class="col-md-2">8</div>
+            <div class="col-md-2">3</div>
+        </div>
+        <div class="row center-block" style="background-color: #adadad">
+            <div class="col-md-2">Taiwan</div>
+            <div class="col-md-2">1</div>
+            <div class="col-md-2">3</div>
+            <div class="col-md-2">2</div>
+            <div class="col-md-2">8</div>
+            <div class="col-md-2">3</div>
+        </div>
+        <div class="row center-block" style="background-color: #adadad">
+            <div class="col-md-2">Taiwan</div>
+            <div class="col-md-2">1</div>
+            <div class="col-md-2">3</div>
+            <div class="col-md-2">2</div>
+            <div class="col-md-2">8</div>
+            <div class="col-md-2">3</div>
+        </div>
     </div>
 </div>
 <div class="jumbotron" style="height: 100%; background-color: #80ff80">
     <div class="container">
-        <h1>Group B</h1>
+        <h2>Group B</h2>
     </div>
 </div>
 <div class="jumbotron" style="height: 100%; background-color: #8080ff">
     <div class="container">
-        <h1>Group C</h1>
+        <h2>Group C</h2>
     </div>
 </div>
 <div class="jumbotron" style="height: 100%; background-color: #ff8080">
     <div class="container">
-        <h1>Group D</h1>
+        <h2>Group D</h2>
     </div>
 </div>
 @stop
