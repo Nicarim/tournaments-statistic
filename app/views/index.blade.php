0c59471035f024f714621243f6d58d5ded973c15
Marcin <marcin19962@onet.eu>
2014-02-03 15:40:18 +0100

    Index template changes

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,5 +1,24 @@
 @extends('master')
 
 @section('content')
-<h1>Asd</h1>
+<div class="jumbotron" style="height: 100%; background-color: #b9ad44">
+    <div class="container">
+        <h1>Group A</h1>
+    </div>
+</div>
+<div class="jumbotron" style="height: 100%; background-color: #80ff80">
+    <div class="container">
+        <h1>Group B</h1>
+    </div>
+</div>
+<div class="jumbotron" style="height: 100%; background-color: #8080ff">
+    <div class="container">
+        <h1>Group C</h1>
+    </div>
+</div>
+<div class="jumbotron" style="height: 100%; background-color: #ff8080">
+    <div class="container">
+        <h1>Group D</h1>
+    </div>
+</div>
 @stop
