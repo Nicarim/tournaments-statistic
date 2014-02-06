dda4511b01d71dce35c2c5a79ea74a0bbcec6f05
Marcin <marcin19962@onet.eu>
2014-02-06 19:31:17 +0100

    Oops, forgot to add to previous commit

---
+++ app/database/migrations/2014_02_06_175426_create_tournaments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTournamentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+        Schema::create('tournaments', function($table){
+            $table->increments('id');
+            $table->string('name');
+            $table->text('overview');
+            $table->integer('slots')->default(0);
+            $table->integer('max_slots');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('tournaments');
+	}
+
+}
---
+++ app/views/tournament/view.blade.php
@@ -0,0 +1,21 @@
+@extends("../master")
+@include("tournament/navbar")
+<div class="container">
+    <h1>osu! Taiko World Cup</h1>
+    <ul class="nav nav-tabs">
+        <li class="active"><a href="#overview" data-toggle="tab">Overview</a></li>
+        <li><a href="#rules"  data-toggle="tab">Rules</a></li>
+        <li><a href="#mappools" data-toggle="tab">Map Pools</a></li>
+        <li><a href="#teams" data-toggle="tab">Teams</a></li>
+    </ul>
+    <div class="tab-content">
+        <div class="tab-pane fade in active" id="overview">
+           <a class="label label-info pull-right" href="#"> edit </a> <!-- edit button -->
+            {{$content}}
+        </div>
+        <div class="tab-pane fade" id="rules">Something</div>
+        <div class="tab-pane fade" id="mappools">Something</div>
+        <div class="tab-pane fade" id="teams">Something</div>
+    </div>
+
+</div>
