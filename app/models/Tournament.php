5186ece748f4f1a1a6b1f3da80eafa67c4067dfe
Loctav <loctav@ppy.sh>
2014-02-07 12:22:45 +0100

    Connect tournament table with group table

--- app/models/Tournament.php
+++ app/models/Tournament.php
@@ -3,5 +3,8 @@
 class Tournament extends Eloquent {
 	protected $guarded = array();
     protected $table = "tournaments";
+        public function groups(){
+        return $this->hasMany('Group')->orderBy('tourneyid')->orderBy('id');
+        }
 	public static $rules = array();
 }
