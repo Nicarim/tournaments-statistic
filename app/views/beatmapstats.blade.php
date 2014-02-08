5014926ac07f3a26c5b9aaaaa5861014da334c9d
Marcin <marcin19962@onet.eu>
2014-02-08 22:43:01 +0100

    Experimental stats for beatmaps

--- app/views/beatmapstats.blade.php
+++ app/views/beatmapstats.blade.php
@@ -3,16 +3,14 @@
     <div class="container">
         <table class="table">
             <tr>
-                <th>ID</th>
                 <th>Pick</th>
                 <th>Name</th>
                 <th>Playcount</th>
             </tr>
             @foreach ($beatmaps as $beatmap)
             <tr>
-                <td>{{$beatmap->beatmap_id}}</td>
                 <td>{{$beatmap->picktype}}</td>
-                <td>{{$beatmap->artist." - ".$beatmap->title." [".$beatmap->diff."]"}}</td>
+                <td><a href="https://osu.ppy.sh/b/{{$beatmap_id}}">{{$beatmap->artist." - ".$beatmap->title." [".$beatmap->diff."]"}}</a></td>
                 <td>{{$beatmap->played}}</td>
             </tr>
             @endforeach
