11a2241ee2a9854251a46c8c44dd877b228b2280
Marcin <marcin19962@onet.eu>
2014-02-08 10:40:01 +0100

    Added SDR (completed stats site)

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -42,8 +42,8 @@ class HomeController extends BaseController {
             $teamBwon = 0; // ^
             $sdr = 0.00; // unused, TO-DO: add max score for each beatmap
             $gameKey = 0; //defines match key which will be iterated over every played match, used for ignoring certain matches in case of nullify
-            foreach($matches as $key => $game){
-                $localjsondata = json_decode(file_get_contents("https://osu.ppy.sh/api/get_match?k=".$this->apikey."&mp=".$game)); // get match data for each match played (in case of multi matches because of... something
+            foreach($matches as $key => $match){
+                $localjsondata = json_decode(file_get_contents("https://osu.ppy.sh/api/get_match?k=".$this->apikey."&mp=".$match)); // get match data for each match played (in case of multi matches because of... something
                 foreach($localjsondata->games as $game){ // iterate over every game played... within match
                     if (!in_array($gameKey,$ignmatches)){
                         $teamAscore = 0; // their overall score for whole match
@@ -62,7 +62,15 @@ class HomeController extends BaseController {
                             $teamBwon += 1;
                         else
                             return "what the fuck?";
+
                         //need to add SDR here
+                        try{
+                        $beatmap = Beatmap::where("beatmap_id",$game->beatmap_id)->first();
+                        $sdr += round(($teamAscore - $teamBscore) / $beatmap->maxscore,3);
+                        }catch (Exception $e){
+                            return $game->beatmap_id;
+                        }
+
                     }
                     $gameKey++;
                 }
@@ -74,22 +82,29 @@ class HomeController extends BaseController {
                 $matchModel->wteam_id = $teamA->id;
                 $matchModel->lteam_id = $teamB->id;
                 $matchModel->room_id = $data['matcheslist'];
+                $matchModel->sdr = $sdr;
                 $matchModel->save();
                 $teamA->mw += 1;//won matches by A
                 $teamB->ml += 1;//lost matches by opposite team
                 $teamA->gw += $teamAwon;//games won by A
                 $teamB->gl += $teamAwon;//lost games by opposite team
+                $teamA->sdr += $sdr;
+                $teamB->sdr += -($sdr);
+
             }elseif($teamAwon < $teamBwon){
                 $matchModel = new Match;
                 $matchModel->group_id = $teamB->group_id;
                 $matchModel->wteam_id = $teamB->id;
                 $matchModel->lteam_id = $teamA->id;
                 $matchModel->room_id = $data['matcheslist'];
+                $matchModel->sdr = $sdr;
                 $matchModel->save();
                 $teamB->mw += 1;//won matches by A
                 $teamA->ml += 1;//lost matches by opposite team
                 $teamB->gw += $teamBwon;//games won by A
                 $teamA->gl += $teamBwon;//lost games by opposite team
+                $teamB->sdr += $sdr;//lost games by opposite team
+                $teamA->sdr += -($sdr);//lost games by opposite team
             }
             $teamA->save(); //save both teams
             $teamB->save(); //^
---
+++ app/models/Beatmap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marcin
+ * Date: 07.02.14
+ * Time: 22:45
+ */
+
+class Beatmap extends Eloquent{
+    protected $table = "beatmaps";
+} 
