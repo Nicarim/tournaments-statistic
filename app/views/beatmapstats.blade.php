4c8f7d2bf69ca226722ad8c871b90311b0982230
Marcin <marcin19962@onet.eu>
2014-02-09 19:46:58 +0100

    Experimental stats for beatmaps

--- app/views/beatmapstats.blade.php
+++ app/views/beatmapstats.blade.php
@@ -10,19 +10,11 @@
             @foreach ($beatmaps as $beatmap)
             <tr>
                 <td>{{$beatmap->picktype}}</td>
-                <td><a href="https://osu.ppy.sh/b/{{$beatmap->id}}">{{$beatmap->artist." - ".$beatmap->title." [".$beatmap->diff."]"}}</a></td>
+                <td><a href="https://osu.ppy.sh/b/{{$beatmap->beatmap_id}}">{{$beatmap->artist." - ".$beatmap->title." [".$beatmap->diff."]"}}</a></td>
                 <td>{{$beatmap->played}}</td>
             </tr>
             @endforeach
         </table>
-        <form role="form" name="input" method="post">
-        <div class="form-group">
-            <label for="password">Password:</label>
-            <input type="text" class="form-control" id="password" name="password" placeholder="You think i'm going to give you a hint?">
-        </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-default">Refresh stats (may take a while)</button>
-            </div>
-        </form>
+
     </div>
 @stop
