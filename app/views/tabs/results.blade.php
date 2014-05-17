5e9b9b43577ac241dfa39d2ad4a651785d64d440
Marcin <marcin19962@onet.eu>
2014-05-17 12:57:12 +0200

    Show basic results of each group and team.

--- app/views/tabs/results.blade.php
+++ app/views/tabs/results.blade.php
@@ -3,6 +3,29 @@
 <div class="container">
     @include('tournament/tourney-navbar')
     <div class="tab-content">
-        Results
+        <h2>{{$tournament->groupStages->first()->name}}</h2>
+        @foreach($tournament->groupStages->first()->group as $group)
+            <h3>{{$group->name}}</h3>
+            <table class="table table-bordered">
+                <tr>
+                    <th>Name</th>
+                    <th>Matches Won</th>
+                    <th>Matches Lost</th>
+                    <th>Games Won</th>
+                    <th>Games Lost</th>
+                    <th>SDR</th>
+                </tr>
+                @foreach($group->teams as $team)
+                <tr>
+                    <td>{{$team->name}}</td>
+                    <td>{{$team->matches_won}}</td>
+                    <td>{{$team->matches_lost}}</td>
+                    <td>{{$team->games_won}}</td>
+                    <td>{{$team->games_lost}}</td>
+                    <td>{{$team->score_difference}}</td>
+                </tr>
+                @endforeach
+            </table>
+        @endforeach
     </div>
 </div>
